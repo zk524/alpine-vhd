3bb8fbddbfd9ac36ae0b5a8628e24302e6e4eb13
zk524 <20262010007@fudan.edu.cn>
2023-01-15 10:19:35 +0800

    script

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 rd /s /q output-hyperv-iso
+rd /s /q packer_cache
 packer.exe build hyperv.json
 
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-apt-get update && apt-get install packer qemu-system-x86-64
+apt-get update && apt-get install -y packer qemu-system-x86-64
+rm -rf output-qemu
+rm -rf packer_cache
 packer build qemu.json
 qemu-img convert -f raw -O vpc -o subformat=fixed,force_size output-qemu/alpine alpine.vhd
